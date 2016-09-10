da31af23bf0e6e3c27c39f469288176565059f76
fan <761822391@qq.com>
2016-09-10 23:41:14 +0800

    修改blog信息为个人信息，并修改bg图片

--- common/config.js
+++ common/config.js
@@ -1,18 +1,18 @@
 var config = {
     blogName: '个人博客',
-    headPicture: '/images/bg.png',
+    headPicture: '/images/bg2.png',
     aPageNum: 7,//分页,一页显示多少文章.
-    dbUser: 'root',//dbUser填写 '' 说明数据库和本博客在同一台机器
-    dbPass: '1234',
-    dbAddress: '111.111.111.111',
+    dbUser: '',//dbUser填写 '' 说明数据库和本博客在同一台机器
+    dbPass: '',
+    dbAddress: 'localhost',
     dbPort: '27017',
     dbName: 'blog',
-    ownerName: '龙门外的鱼',//个人姓名，显示在博主信息中
-    ownerLocation: '',//所在地，显示在博主信息中
-    ownerOccupation: '-',//职业，显示在博主信息中
-    ownerSkill: '',//主要从事领域与技能，显示在博主信息中
-    motto: 'Thoughts, stories and ideas.',//座右铭，显示在博主信息中
-    email: 'longmenwaideyu@126.com',
+    ownerName: 'fantaosong',//个人姓名，显示在博主信息中
+    ownerLocation: '深圳 中国',//所在地，显示在博主信息中
+    ownerOccupation: 'Java开发工程师',//职业，显示在博主信息中
+    ownerSkill: 'Java SpringMVC NLP Machine Learning',//主要从事领域与技能，显示在博主信息中
+    motto: '静观世界求一事，诚对苍生不二心',//座右铭，显示在博主信息中
+    email: 'fantaosong1988@gmail.com',
     serverPlatform: {
         platform: 'local',
         AccessKey: 'xxx',//platform是local可以不填
@@ -22,16 +22,17 @@ var config = {
     mailConfig: {
         service: 'Gmail',//Gmail QQ QQex Yahoo Hotmail
         auth: {
-            user: 'longmenwaideyu@gmail.com',
-            pass: 'abcd'
+            user: 'fantaosong1988@gmail.com',
+            pass: 'xxxx'
         }
     },
     otherBlog: {//你的其他博客，显示在页脚
-        url: 'http://hi.baidu.com/longmenwaideyu',
-        name: '百度博客'
+        url: 'https://github.com/fantaosong',
+        name: 'Github'
+
     },
     friendlyLinks: [//友情链接，显示在页脚
-        { url: 'http://hi.baidu.com/longmenwaideyu', name : '百度博客' }
+        { url: 'https://github.com/fantaosong', name : 'GitHub' }
     ],
     ICPNumber: '京ICP备00000000号'//显示在页脚
 }
